7e65f576a1a42d28d412e075d417dca8d84f32ae
IGRX <IGRXe@outlook.com>
2023-11-26 11:44:27 +0800

    将触发方式改为

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,26 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  ctx.on('message', (session) => {
-    if (session.content === 'cfr') {
-      let now_message_status = [session.event.platform, session.event.message.id, session.event.channel.id, session.event.guild.id, session.event.user.id, session.selfId]
-      session.send('平台名: '+now_message_status[0]+'\n'+'消息ID: '+now_message_status[1]+'\n'+'频道ID: '+now_message_status[2]+'\n'+'群组ID: '+now_message_status[3]+'\n'+'用户ID: '+now_message_status[4]+'\n'+'事件自身ID: '+now_message_status[5]+'\n此状态来自于cfri-test插件')
-    }
-  })
+
+  ctx.command('cfr','获取当前状态，由cfri-test插件提供', { authority: 1 })
+  .action(async ({session}) => {
+      try {
+        let now_message_status = []
+        now_message_status[0] = await session.event.platform
+        now_message_status[1] = await session.event.message.id
+        now_message_status[2] = await session.event.channel.id
+        now_message_status[3] = await session.event.guild.id
+        now_message_status[4] = await session.event.user.id
+        now_message_status[5] = await session.selfId
+        session.send('平台名: '+now_message_status[0]+'\n'
+        +'消息ID: '+now_message_status[1]+'\n'
+        +'频道ID: '+now_message_status[2]+'\n'
+        +'群组ID: '+now_message_status[3]+'\n'
+        +'用户ID: '+now_message_status[4]+'\n'
+        +'事件自身ID: '+now_message_status[5]
+        +'\n此状态来自于cfri-test插件')
+      } catch (error) {
+        session.send('获取状态失败')
+      }
+    })
 }
