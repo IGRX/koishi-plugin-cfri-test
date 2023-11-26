70dee177c46559dc44380344f9ba3c2e02c59885
IGRX <IGRXe@outlook.com>
2023-11-26 18:17:20 +0800

    Add debug information to get_info_status function
    Update get_info_status function to include tracker information

--- src/get_status_info.ts
+++ src/get_status_info.ts
@@ -13,23 +13,25 @@ export function get_info_status(ctx: Context) {
   ctx.command('cfr.id', '获取用户ID', { authority: 1 })
   .action(async ({session}) => {
     try {
-      return session.send('用户ID: ' + session.event.user.id)
+      session.send('用户ID: ' + session.event.user.id)
+      session.send('\nTracker信息:\n当前指令监听器Event为： ' + session.type + '\n来自用户：' + session.username)//新增debug用信息
     } catch (error) {
-      return session.send('获取用户ID失败，请联系频道主')
+      session.send('获取用户ID失败，请联系频道主')
     }
   })
   ctx.command('cfr.all', '获取所有状态信息', { authority: 1 })//你妈，一个主指令里只能注册一个子指令，我还以为可以多个呢，如果用subcommand的话，那么这个子指令不会被注册，因为前面已经有一个.id子指令了，还是用这种方法注册吧
   .action(async ({session}) => {
     try {
-      return session.send('平台名: ' + session.event.platform + '\n'
+      session.send('平台名: ' + session.event.platform + '\n'
       + '消息ID: ' + session.event.message.id + '\n'
       + '频道ID: ' + session.event.channel.id + '\n'
       + '群组ID: ' + session.guildId + '\n'
       + '用户ID: ' + session.event.user.id + '\n'
       + '事件自身ID: ' + session.selfId
       + '\n此状态来自于cfri-test插件')
+      session.send('\nTracker信息:\n当前指令触发事件为： ' + session.type + '\n来自用户：' + session.username)//新增debug用信息
     } catch (error) {
-      return session.send('获取状态信息失败，请联系频道主')
+      session.send('获取状态信息失败，请联系频道主')
     }
   })
   try {
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Context, Schema,h } from 'koishi'
 import { get_info_status } from './get_status_info'
 
+
 export const name = 'cfr-test'
 
 export interface Config { }
@@ -9,5 +10,4 @@ export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
   get_info_status(ctx)//get_info_status.ts里写了查询一堆状态的函数，这里用import导入,主ts就简洁多了，下面就是画的大饼（逃
-  
 }
